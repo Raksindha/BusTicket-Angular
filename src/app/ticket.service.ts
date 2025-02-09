2088fad50e7a0bf62339200c17d5bd776ada485b
raksindha <raksheen9899@gmail.com>
2025-02-09 17:17:29 +0530

    cross issue fix for location search

--- src/app/ticket.service.ts
+++ src/app/ticket.service.ts
@@ -2,16 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+// ticket.service.ts
 @Injectable({
   providedIn: 'root'
 })
 export class TicketService {
-
-  private apiURL: string = 'https://projectapi.gerasim.in/api/BusBooking/';  // Added trailing slash
+  private apiURL: string = '/api/BusBooking/';  // Remove the domain
 
   constructor(private http: HttpClient) { }
 
   getLocations(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiURL + "GetBusLocations");  // Now correctly concatenated
+    return this.http.get<any[]>(this.apiURL + "GetBusLocations");
   }
 }
